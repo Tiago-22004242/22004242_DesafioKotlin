426ec949a2d4c844163899f62053c0aa574b6617
tiago <a22004242@alunos.ulht.pt>
2023-03-20 22:08:23 +0000

    Adicionar interfaces

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -1,12 +1,12 @@
 package pt.ulusofona.cm.kotlin.challenge.models
-import java.time.LocalDate
+import java.util.Date
 
 
 abstract class Veiculo (open val identificador : String){
     var posicao : Posicao = Posicao(0,0)
-    var dataDeAquisicao : LocalDate = LocalDate.now()
+    var dataDeAquisicao : Date = Date()
     fun setDataAquisicao() {
-        dataDeAquisicao = LocalDate.now()
+        dataDeAquisicao = Date()
     }
     abstract fun requerCarta() : Boolean
 }
