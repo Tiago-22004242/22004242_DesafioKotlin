9db49c9299cc626607606b2aadd29fec55e27402
Tiago-22004242 <a22004242@alunos.ulht.pt>
2023-03-21 09:24:04 +0000

    Mover Para

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,9 +1,8 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
-import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.text.SimpleDateFormat
 
-class Carro(override val identificador : String, val motor : Motor) : Veiculo(identificador){
+class Carro(override val identificador : String, val motor : Motor) : Veiculo(identificador),Ligavel{
     override fun requerCarta(): Boolean {
        return true
     }
@@ -14,6 +13,18 @@ class Carro(override val identificador : String, val motor : Motor) : Veiculo(id
         val formato = SimpleDateFormat("dd-MM-yyyy")
         val dataModificada = formato.format(dataDeAquisicao)
         return dataModificada.toString()
+    }
+
+    override fun ligar() {
+        motor.ligar()
+    }
+
+    override fun desligar() {
+        motor.desligar()
+    }
+
+    override fun estaLigado(): Boolean {
+        return motor.estaLigado()
     }
 
     override fun toString(): String {
--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -1,5 +1,4 @@
 package pt.ulusofona.cm.kotlin.challenge.models
-import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.util.Date
 
